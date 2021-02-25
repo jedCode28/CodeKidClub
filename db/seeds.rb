7d570b45f5b18deab32554992734d52b3f1f7f4f
Jedediah Wood <jedediahwood28@gmail.com>
2021-02-25 14:41:01 -0700

    pushup after seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Department.destroy_all
+Item.destroy_all
 3.times do 
     Department.create(name: Faker::Coffee.blend_name, body: Faker::Coffee.notes)
+    3.times do 
+      Item.create(name: Faker::Beer , body: Faker::Dessert)
+    end 
 end
 puts "seeded #{Department.length}"
+
+
+
